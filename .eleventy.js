3f4ed0436784db544cb124c9894ddcee11dfd70c
Magnus Junghard Huttu <magnushuttu@gmail.com>
2022-09-29 17:19:12 +0200

    remove css minifier

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,4 @@
-const CleanCSS = require('clean-css')
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addFilter('cssmin', function (code) {
-    return new CleanCSS({}).minify(code).styles
-  })
   eleventyConfig.addPassthroughCopy('./src/css')
   eleventyConfig.addWatchTarget('./src/css')
   return {
